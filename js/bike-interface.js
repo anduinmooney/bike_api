5de2934410cbd8fa580d4bac31a85e351662d53e
Anduin Mooney and Zach Evans <zach@>
2018-02-14 13:56:02 -0800

    Initial commit (sorry).

---
+++ js/bike-interface.js
@@ -0,0 +1,25 @@
+import { promise } from './../js/bike.js';
+
+$(document).ready(function() {
+  let distance;
+  let location;
+  let perPage;
+
+  $('#bike-form').submit(function(event) {
+    event.preventDefault();
+    let distance = $('#distance').val();
+    let location = $('#location').val();
+    let perPage = $('#per-page').val();
+    let newSearch = promise(perPage, location, distance);
+
+    newSearch.then(function(response) {
+      let body = JSON.parse(response);
+      $('#result').text(body.bikes[0].title);
+      // for (let i = 0; body.bikes.length; i++) {
+      //   $('#result').text(body.bikes[i]);
+      // }
+    }, function(error) {
+      $('.showErrors').text(`There was an error: ${error.message}`);
+    });
+  });
+});
